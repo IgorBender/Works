312f22b199ad9d65790b6c9e76a134b9b2413656
Igor <benigor@gmail.com>
2019-03-26 17:39:14 +0200

    Add prints to MoveTest.cpp

--- EclipseWorkspace/MoveTest/MoveTest.cpp
+++ EclipseWorkspace/MoveTest/MoveTest.cpp
@@ -13,8 +13,19 @@ using namespace std;
 class TestClass
 {
 public:
-    TestClass() {}
-    ~TestClass() {}
+    TestClass()
+    {
+        cout << "TestClass instance is created." << endl;
+    }
+    ~TestClass()
+    {
+        cout << "TestClass instance is destroyed." << endl;
+    }
+    TestClass& operator=(TestClass& other)
+    {
+        m_Int = other.m_Int;
+        return *this;
+    }
 
     void setInt(uint32_t Int)
     {
@@ -36,7 +47,7 @@ public:
     MoveTestClass() { m_pTestClass = new TestClass; }
     // Copy-constructor.
     MoveTestClass(const MoveTestClass& other) {
-        m_pTestClass = new TestClass; // Create new intwrnal resource.
+        m_pTestClass = new TestClass; // Create new internal resource.
         *m_pTestClass = *(other.m_pTestClass); // Copy internal resource.
     }
     // Move-constructor.
@@ -72,13 +83,19 @@ protected:
     TestClass* m_pTestClass = nullptr;
 };
 
-int main(int argc, char *argv[])
+int main(/*int argc, char *argv[]*/)
 {
+    cout << "Create MoveTestClass instance t1, includes TestClass instance." << endl;
     MoveTestClass t1;
     t1.setInt(3);
+    cout << "Create MoveTestClass instance t2, includes full copy of t1 internal TestClass instance (via copy-constructor)." << endl;
     MoveTestClass t2(t1); // t2 is copy of t1.
+    cout << "Create MoveTestClass instance t3, includes instance of t1 (via move-constructor)." << endl;
     MoveTestClass t3(move(t1)); // t3 contains resource of t1. t1 don't contain resource.
+    cout << "Create MoveTestClass instance t4, includes own TestClass instance." << endl;
     MoveTestClass t4;
+    cout << "Move TestClass instance of t2 into t4, TestClass instance of t4 will be destroyed." << endl;
     t4 = move(t2); // t4 free its internal resource and gets t2 internal resource, t2 don't contain resource.
+    cout << "Destroy t3 and t4 internal instances." << endl;
     return 0;
 }
